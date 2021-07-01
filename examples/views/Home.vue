f2383f388181165a6342fca23aa4f6b562c64a94
yfhu <879649442@qq.com>
2021-07-01 16:55:30 +0800

    0.0.1

---
+++ examples/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+  components: {},
+
+  methods: {
+
+  },
+};
+</script>
+<style scoped lang="scss">
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+</style>
