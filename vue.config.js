9fe707be73877486d720a4274ef71718db726684
yfhu <879649442@qq.com>
2021-09-30 13:21:10 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
   //部署应用包时的基本 URL
   // publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
   //输出路劲
-  // outputDir: "dist",
+  outputDir: "ven-ui",
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
   // assetsDir: "",
   //指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径
